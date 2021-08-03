cf323dad5936c0657f05bc2b96c4eba9725130c8
brandon <bberning.web@gmail.com>
2021-08-03 13:05:21 -0400

    removed boilerplate files that come with create react app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import {Auth, API, graphqlOperation} from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 import awsconfig from './aws-exports';
@@ -15,22 +14,10 @@ function App() {
 
   const allMaps = API.graphql(graphqlOperation(queries.listMaps))
   console.log(allMaps)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
     </div>
   );
 }
